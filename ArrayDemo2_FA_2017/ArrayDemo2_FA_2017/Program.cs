8c88241651b3cf5b9073182760a58a81f814e3a0
Shub, Dmitry <dshub@colum.edu>
2018-10-31 18:16:57 -0500

    test

---
+++ ArrayDemo2_FA_2017/ArrayDemo2_FA_2017/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo2_FA_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an array to hold grocery list values
+            //string[] items = new string[5];
+
+            //declare a new array and assign values in one line of code
+            string[] items = new string[] { "cereal","milk","eggs","orange juice","pancake mix"};
+
+            //populate the array with data (values)
+            //items[0] = "cereal";
+            //items[1] = "milk";
+            //items[2] = "eggs";
+            //items[3] = "orange juice";
+            //items[4] = "pancake mix";
+
+            //print out the grocery list items by pulling from the array
+            Console.WriteLine("Items on my grocery list include:\n");
+            Console.WriteLine(items[0]);
+            Console.WriteLine(items[1]);
+            Console.WriteLine(items[2]);
+            Console.WriteLine(items[3]);
+            Console.WriteLine(items[4]);
+
+            Console.ReadLine();
+        }
+    }
+}
